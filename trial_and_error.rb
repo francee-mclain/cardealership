77ce1fbc4bf7f6ce2d97ce1c235f2eacb60de1a3
Kylie <chiixil.84@gmail.com>
2020-07-13 19:00:37 -0500

    hopefully this works!!

--- trial_and_error.rb
+++ trial_and_error.rb
@@ -1,16 +1,25 @@
 class Car
-        def initialize(make, model, year)
+    def initialize(make, model, year, mileage, eng, dt)
         @make = make.capitalize
         @model = model.capitalize
         @year = year.capitalize
+        @mileage = mileage
+        @eng = eng.capitalize
+        @dt = dt.upcase
     end
    
     def to_sales
-        "We have a #{@year} #{@make} #{@model} on our lot."
+        "We have a #{@year} #{@make} #{@model} on our lot. It has #{@mileage} miles, is a #{@eng} engine, and is a #{@dt}."
     end
     
 end
 
-car1 = Car.new("Toyota", "Corolla", "1994")
+car1 = Car.new("Toyota", "Corolla", "1994", 230000, "V4", "FWD")
+@make = "Toyota"
+@model = "Corolla"
+@year = "1994"
+@mileage = 230000
+@eng = "V4"
+@dt = "FWD"
 
 puts car1.to_sales
