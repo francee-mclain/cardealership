ca73637deda3393982ec81417e932e23a012e1b3
Kylie <chiixil.84@gmail.com>
2020-07-16 17:54:03 -0500

    more stuff added for the "questions

--- trial_and_error.rb
+++ trial_and_error.rb
@@ -1,15 +1,18 @@
 cars_list = []
 
 class Car
-    def initialize(make = "", model = "", year = "", eng = "")
+    def initialize(make = "", model = "", year = "", eng = "", dt = "", mileage = "", price = "")
         @make = make.capitalize
         @model = model.capitalize
-        @year = year.capitalize
+        @year = year
         @eng = eng.capitalize
+        @dt = dt.upcase
+        @mileage = mileage
+        @price = price
     end
    
     def to_sales
-        "We have a #{@year} #{@make} #{@model} on our lot. It has a #{@eng} engine."
+        "We have a #{@year} #{@make} #{@model} on our lot. It has a #{@eng} engine, which drives using a #{@dt}. Its mileage is #{@mileage}. The price is #{@price}."
     end
 end
 
@@ -44,6 +47,24 @@ def add_eng
     return eng
 end
 
+def add_dt
+    dt = {"dt" => ""}
+    dt["dt"] = ask("What is the vehicle's drive train?")
+    return dt
+end
+
+def add_mileage
+    mileage = {"mileage" => ""}
+    mileage["mileage"] = ask("What is the vehicle's mileage?")
+    return mileage
+end
+
+def add_price
+    price = {"price" => ""}
+    price["price"] = ask("What is the vehicle's price?")
+    return price
+end
+
 answer  = ""
 while answer != "n"
     make = add_make()
@@ -53,25 +74,30 @@ while answer != "n"
     year = add_year()
     answer = ask("Would you like to add its engine type? (y/n)")
     eng = add_eng()
+    answer = ask("Would you like to add its drive train? (y/n)")
+    dt = add_dt()
+    answer = ask("Would you like to add its mileage? (y/n)")
+    mileage = add_mileage()
+    answer = ask("Would you like to add its price? (y/n)")
+    price = add_price()
     answer = ask("Is this everything? (y/n)")
-    if answer = "y"
+    if answer == "y"
         then answer = "n"
-    cars_list.push(Car.new(make["make"], model["model"], year["year"], eng["eng"]))
+    cars_list.push(Car.new(make["make"], model["model"], year["year"], eng["eng"], dt["dt"], mileage["mileage"], price["price"]))
 end
 
 puts "---"
 
 
-# car1 = Car.new("Toyota", "Corolla", "1994", 230000, "V4", "FWD", 2000)
+# car1 = Car.new("Toyota", "Corolla", "1994", "V4", "FWD", 2000, 230000)
 # @make = "Toyota"
 # @model = "Corolla"
 # @year = "1994"
-# @mileage = 230000
 # @eng = "V4"
 # @dt = "FWD"
 # @price = 2000
-
-# puts car1.to_sales
+# @mileage = 230000
 
 puts cars_list[0].to_sales()
+
 end
