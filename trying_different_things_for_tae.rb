3b122da10e432c317fd218353a4f5c24e255c774
Kylie <chiixil.84@gmail.com>
2020-07-16 19:51:53 -0500

    much better!!!

--- trying_different_things_for_tae.rb
+++ trying_different_things_for_tae.rb
@@ -4,19 +4,21 @@ class Car
     attr_accessor :make, :model, :year
 
     def initialize(make, model, year)
-        @make = make.capitalize
-        @model = model.capitalize
+        @make = make
+        @model = model
         @year = year
     end
 
     def to_s
-        "We have a #{@year} #{@make} #{@model} on our lot."
+        "#{@year} #{@make} #{@model}"
     end
 
+    car1 = Car.new(make = "Toyota", model = "Corolla", year = 1994)
+    car2 = Car.new(make = "Toyota", model = "Corolla", year = 1999)
+    car3 = Car.new(make = "Mini Cooper", model = "S", year = 2006)
+    
+    cars_list = [car1, car2, car3]
+
+    puts "We have a #{car1.to_s}, #{car2.to_s}, and #{car3.to_s} on our used car lot."
+
 end
-
-car1 = Car.new(make = "Toyota", model = "Corolla", year = 1994)
-
-cars_list = [car1]
-
-puts cars_list.to_s
